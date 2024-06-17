ff992a02a57f2610e99a668fe240124dd40bd988
AiTrinh303 <trong.trinh@dci-student.org>
2024-06-17 13:03:36 +0200

    done

--- .history/vite.config_20240617130201.js
+++ .history/vite.config_20240617130201.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+})
+
+export default defineConfig({
   build: {
     outDir: 'dist', // Default output directory
   },
-})
-
+});
